61e417b7b3940f184749c55823aeed726238dbee
gmasmejean <guillaume.masmejean@gmail.com>
2018-02-13 16:11:43 +0100

    Adding redirection support on box.upload + logs

--- api/methods/box_upload.js
+++ api/methods/box_upload.js
@@ -3,6 +3,7 @@ var http = require('http'),
     URL = require('url');
 
 module.exports = function( app ){
+    max_redirection = 1;
 
     function sendBoxResult( err, file_id, box_id ){
         var api_conf = app.configuration.services.twic_api,
@@ -17,17 +18,88 @@ module.exports = function( app ){
                 'Content-Length': body.length
             },api_conf.headers),
             path: api_conf.box_upload_path
-        }/*function(r){
+        },function(r){
             var d='';
             r.on('data', c => d+=c );
             r.on('end', () => console.log('API OUTPUT', d) );
-        }*/);
+        });
 
         request.on('error', err => {
             app.logger.error('TWIC API "'+api_conf.method+'" ERR', err.message );
         });
         request.write( body );
         request.end();
+    }
+
+    function onStatusOK( result, params ){
+        var data,
+            infos = {name:params.name,parent:{id:'0'}},
+            delimiter = '-------------9846zadzapoihu',
+            bodyStart = '--'+delimiter+'\r\nContent-Disposition: form-data; name="attributes"\r\n\r\n'+JSON.stringify(infos)+'\r\n'+
+                '--'+delimiter+'\r\nContent-Disposition: form-data; name="file"; filename="file"\r\n\r\n',
+            bodyEnd = '\r\n--'+delimiter+'--\r\n',
+            options = {
+                hostname: app.configuration.services.box.host,
+                path: app.configuration.services.box.path,
+                headers:{
+                    'Transfer-Encoding': 'chunked',
+                    'Authorization': 'Bearer '+app.configuration.services.box.api_key,
+                    'Content-Type':'multipart/form-data; boundary='+delimiter,
+                    'Content-Length': result.headers['content-length'] + bodyStart.length + bodyEnd.length
+                },
+                method: 'POST',
+                rejectUnauthorized: false,
+            };
+        
+        var boxRequest = https.request(options, response => {
+            let d='';
+            response.on('data', chunk => d+=chunk );
+            response.on('end', ()=> {
+                try{
+                    console.log('Box:',d);
+                    var data = JSON.parse(d);
+                    if( data.entries && data.entries[0] && data.entries[0].id ){
+                        sendBoxResult(undefined, params.id, data.entries[0].id);
+                    }
+                }catch( e ){
+                    app.logger.error('BOX RESULT PARSE ERR', e.message );
+                }
+            });
+        }).on('error', e => {
+            app.logger.error('BOX UPLOAD ERR', e.message );
+            sendBoxResult(e.message);
+        });
+        // SEND BODY START
+        boxRequest.write( bodyStart );
+        // ON DATA -> SEND DATA TO BOX...
+        result.on('data', chunk => { 
+            boxRequest.write( chunk );
+        });
+        // ON END -> SEND REQUEST END TO BOX...
+        result.on('end', () => {
+            boxRequest.write( bodyEnd );
+            boxRequest.end();
+        });
+    }
+
+    function onResult( next, params, redirected, result ){
+        if( result.statusCode === 200 ){
+            next( null, true );
+            onStatusOK( result, params );
+        }else if( (result.statusCode === 302 || result.statusCode === 301) 
+            && result.headers.location && redirected < max_redirection ){
+            getFile( result.headers.location, next, params, redirected+1 );            
+        }else{
+            next( app.jsonrpcHandler.errors.INVALID_PARAMS, null);
+        }
+    }
+
+    function getFile( url, next, params, redirected ){
+        (URL.parse( url ).protocol === 'https:'?https:http)
+            .get( url, onResult.bind(null,next,params,redirected))
+            .on('error', () => {
+                next( app.jsonrpcHandler.errors.INVALID_PARAMS, null);
+            });
     }
 
     return {
@@ -39,65 +111,7 @@ module.exports = function( app ){
             }
         },
         method: function(params, next){
-            var url = URL.parse( params.url );
-
-            (url.protocol === 'https:'?https:http).get( params.url, (result) => {
-                if( result.statusCode === 200 ){
-                    next( null, true );
-
-                    var data,
-                        infos = {name:params.name,parent:{id:'0'}},
-                        delimiter = '-------------9846zadzapoihu',
-                        bodyStart = '--'+delimiter+'\r\nContent-Disposition: form-data; name="attributes"\r\n\r\n'+JSON.stringify(infos)+'\r\n'+
-                            '--'+delimiter+'\r\nContent-Disposition: form-data; name="file"; filename="file"\r\n\r\n',
-                        bodyEnd = '\r\n--'+delimiter+'--\r\n',
-                        options = {
-                            hostname: app.configuration.services.box.host,
-                            path: app.configuration.services.box.path,
-                            headers:{
-                                'Transfer-Encoding': 'chunked',
-                                'Authorization': 'Bearer '+app.configuration.services.box.api_key,
-                                'Content-Type':'multipart/form-data; boundary='+delimiter,
-                                'Content-Length': result.headers['content-length'] + bodyStart.length + bodyEnd.length
-                            },
-                            method: 'POST',
-                            rejectUnauthorized: false,
-                        };
-                    
-                    var boxRequest = https.request(options, response => {
-                        let d='';
-                        response.on('data', chunk => d+=chunk );
-                        response.on('end', ()=> {
-                            try{
-                                var data = JSON.parse(d);
-                                if( data.entries && data.entries[0] && data.entries[0].id ){
-                                    sendBoxResult(undefined, params.id, data.entries[0].id);
-                                }
-                            }catch( e ){
-                                app.logger.error('BOX RESULT PARSE ERR', e.message );
-                            }
-                        });
-                    }).on('error', e => {
-                        app.logger.error('BOX UPLOAD ERR', e.message );
-                        sendBoxResult(e.message);
-                    });
-                    // SEND BODY START
-                    boxRequest.write( bodyStart );
-                    // ON DATA -> SEND DATA TO BOX...
-                    result.on('data', chunk => { 
-                        boxRequest.write( chunk );
-                    });
-                    // ON END -> SEND REQUEST END TO BOX...
-                    result.on('end', () => {
-                        boxRequest.write( bodyEnd );
-                        boxRequest.end();
-                    });
-                }else{
-                    next( app.jsonrpcHandler.errors.INVALID_PARAMS, null);
-                }
-            }).on('error', () => {
-                next( app.jsonrpcHandler.errors.INVALID_PARAMS, null);
-            });
+            getFile( params.url, next, params, 0 );
         }
     };
 };
