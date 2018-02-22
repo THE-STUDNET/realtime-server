af50d62e6a2c4fc86373d0c8324ebc20d848474c
gmasmejean <guillaume.masmejean@gmail.com>
2018-02-22 17:09:26 +0100

    Add chat events handling

--- websocket/services/chat.js
+++ websocket/services/chat.js
@@ -1,3 +1,23 @@
 module.exports = function( app, socket ){
-    // NOTHING :P
+    var server = app.websocketServer;
+
+    // Listen to chat events
+    socket.on('ch.writing', _sendWriting );
+    socket.on('ch.read', _sendRead );
+
+    function _sendRead( data ){
+        if( data.users && data.id ){
+            data.users.forEach( user_id => {
+                server.to('user.'+user_id).emit('ch.read', { id:data.id, user_id: socket.user.id });
+            });
+        }
+    }
+
+    function _sendWriting( data ){
+        if( data.users && data.id ){
+            data.users.forEach( user_id => {
+                server.to('user.'+user_id).emit('ch.writing', { id:data.id, user_id: socket.user.id });
+            });
+        }
+    }
 };
